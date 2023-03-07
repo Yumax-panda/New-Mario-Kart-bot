b1bb42d1b16ac66b96a5ccff43328bc2dd92e907
Yumax-panda <93650251+Yumax-panda@users.noreply.github.com>
2023-03-07 21:31:49 +0900

    Update utils.py

--- common/utils.py
+++ common/utils.py
@@ -179,3 +179,9 @@ def maybe_param(txt: str) -> tuple[Optional[str], Optional[int], Optional[str]]:
         return None, None, fc
 
     return txt, None, None
+
+
+async def update_sokuji(data: dict, user_ids: set[str]) -> None:
+    db = deta.AsyncBase('sokuji')
+    await asyncio.gather(*[asyncio.create_task(db.put(data=data, key=user_id)) for user_id in user_ids])
+    await db.close()
